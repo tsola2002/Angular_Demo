821f5d18e376b09bd66f9975c7c7b7c2f6dbf645
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2014-06-10 10:01:54 +0100

    supplying & accessing variable scope

---
+++ js/controllers/app.js
@@ -0,0 +1,39 @@
+/**
+ * Created by solidstunna101 on 10/06/14.
+ */
+
+/*function signature allows angular to pull the scope object look at it first.
+ angular will also call the function
+ */
+function AppCtrl ($scope) {
+    /*adding javascript airport object*/
+    $scope.airports = {
+        "PDX": {
+            "code": "PDX",
+            "name": "Portland International Airport",
+            "city": "Portland",
+            "destinations": [
+                "LAX",
+                "SFO"
+            ]
+        },
+        "STL": {
+            "code": "STL",
+            "name": "Lambert-St. Louis International Airport",
+            "city": "St. Louis",
+            "destinations": [
+                "LAX",
+                "MKE"
+            ]
+        },
+        "MCI": {
+            "code": "MCI",
+            "name": "Kansas City International Airport",
+            "city": "Kansas City",
+            "destinations": [
+                "LAX",
+                "DFW"
+            ]
+        }
+    };
+}
